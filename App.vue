fd963c0164c418e68314b6c9d2abea1ff0075244
cnnmaniac <137510947+cnnmaniac@users.noreply.github.com>
2023-10-10 21:07:40 +0530

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <router-view name="NavBar"></router-view>
+    <router-view name="StudioNavBar"></router-view>
+    <v-content
+      :class="{
+        'content-bg': ![
+          'SignIn',
+          'SignUp',
+          'Dashboard',
+          'Video',
+          'Detail'
+        ].includes(this.$route.name)
+          ? true
+          : false
+      }"
+    >
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
